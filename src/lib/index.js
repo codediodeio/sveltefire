46abf3a8a2c383084148978163cc3129670cb849
Jeff Delaney <hello@fireship.io>
2022-12-20 16:30:56 -0700

    fix missing export

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,12 +2,14 @@
 import User from './User.svelte';
 import Collection from './Collection.svelte';
 import Doc from './Doc.svelte';
+import FirebaseApp from './FirebaseApp.svelte';
 import { docStore, userStore, collectionStore } from './stores';
 
 export {
     Doc,
     User,
     Collection,
+    FirebaseApp,
     docStore,
     collectionStore,
     userStore
