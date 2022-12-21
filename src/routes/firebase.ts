388f1ef520f4486e6e4041d2174107b34bfed648
Jeff Delaney <hello@fireship.io>
2022-12-20 21:28:51 -0700

    rm config

--- src/routes/firebase.ts
+++ src/routes/firebase.ts
@@ -4,12 +4,7 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBOL3Y80Y6JASfwgvyq8IL6FlnrBQBN_0E",
-    authDomain: "testing-proj-b7c60.firebaseapp.com",
-    projectId: "testing-proj-b7c60",
-    storageBucket: "testing-proj-b7c60.appspot.com",
-    messagingSenderId: "665240273060",
-    appId: "1:665240273060:web:b81550e114a46af9116631"
+
 };
 
 // Initialize Firebase
