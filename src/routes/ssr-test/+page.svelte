bcb41277cc000df4741fe256f8fe025b901f9ae4
codediodeio <hello@fireship.io>
2023-07-24 21:49:44 -0700

    test: update ssr test

--- src/routes/ssr-test/+page.svelte
+++ src/routes/ssr-test/+page.svelte
@@ -4,10 +4,10 @@
   export let data;
 </script>
 
-<h1>Not Hydrated: {data.title}</h1>
+<h1>Not Hydrated: {data?.title}</h1>
 
-<Doc ref="posts/first-post" startWith={data} let:data={realtimeData}>
-    <h1>Hydrated: {realtimeData.title}</h1>
+<Doc ref="posts/test" startWith={data} let:data={realtimeData}>
+    <h1>Hydrated: {realtimeData?.title}</h1>
 </Doc>
 
 <p>
--- src/routes/ssr-test/+page.ts
+++ src/routes/ssr-test/+page.ts
@@ -2,7 +2,7 @@ import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../firebase.js';
 
 export const load = (async () => {
-    const ref = doc(db, 'posts', 'first-post');
+    const ref = doc(db, 'posts', 'test');
     const docSnap = await getDoc(ref);
     const data = docSnap.data();
     return {
