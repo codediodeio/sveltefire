fac75af0d430e56d8c7fd5531ca031b50ac5f514
Janny221199 <mail@janny.io>
2023-07-29 12:44:01 +0200

    showing a loading Spinner if the auth status was not fetched on initial page load

---
+++ src/lib/components/AuthLoading.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { userStore } from "../stores/auth.js";
+</script>
+
+{#if $user === undefined}
+  <slot {auth}>Loading...</slot>
+{/if}
--- src/lib/components/SignedOut.svelte
+++ src/lib/components/SignedOut.svelte
@@ -12,6 +12,6 @@
 
   </script>
   
-  {#if !$user}
+  {#if $user === null}
     <slot {auth} />
   {/if}
