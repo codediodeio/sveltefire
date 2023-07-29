58fc9b38603c84a4567487d2157e9ab9732ea15c
Janny221199 <mail@janny.io>
2023-07-29 12:49:12 +0200

    auth loading on first page load:fix missing imports

--- src/lib/components/AuthLoading.svelte
+++ src/lib/components/AuthLoading.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import { getFirebaseContext } from "../stores/sdk.js";
   import { userStore } from "../stores/auth.js";
+  const auth = getFirebaseContext().auth!;
+  const user = userStore(auth);
 </script>
 
 {#if $user === undefined}
