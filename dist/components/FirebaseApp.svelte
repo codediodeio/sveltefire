80af67a72f350b6087797423dcf626e688b7bbb2
codediodeio <hello@fireship.io>
2023-12-17 15:45:02 -0700

    rm cached dist folder artifacts

--- dist/components/FirebaseApp.svelte
+++
@@ -1,9 +0,0 @@
-<script>import { setFirebaseContext } from "../stores/sdk.js";
-export let firestore;
-export let rtdb;
-export let auth;
-export let storage;
-setFirebaseContext({ firestore, rtdb, auth, storage });
-</script>
-
-<slot />
